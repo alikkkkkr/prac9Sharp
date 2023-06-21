8afb9313596c3a8a01a1ee6c2cb3ea20097134ce
алик <113428476+alikkkkkr@users.noreply.github.com>
2023-06-21 12:19:33 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prac9Sharp
+{
+    // alik.ryzhkova@mail.ru
+    // N7xXnksr2S0puC7dufSH
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_vxod_Click(object sender, RoutedEventArgs e)
+        {
+            ImapHelper.Initialize((typePochta_cmbBox.SelectedItem as ComboBoxItem).Tag.ToString());
+            if (ImapHelper.Login(pocha_txt.Text, password_txt.Password))
+            {
+                new Pochta().Show();
+                Close();
+            }
+        }
+    }
+}
---
+++ Pochta.xaml.cs
@@ -0,0 +1,79 @@
+﻿using ImapX;
+using ImapX.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace prac9Sharp
+{
+    // alik.ryzhkova@mail.ru
+    // N7xXnksr2S0puC7dufSH
+
+    /// <summary>
+    /// Логика взаимодействия для Pochta.xaml
+    /// </summary>
+    public partial class Pochta : Window
+    {
+        private CommonFolderCollection folders = ImapHelper.GetFolders();
+        private List<string> nameFolders = new List<string>();
+        private List<string> nameMessage = new List<string>();
+        MessageCollection messages;
+        
+        public Pochta()
+        {
+            InitializeComponent();
+
+            FoldersOut();
+        }
+
+        private void FoldersOut()
+        {
+            foreach (var folder in folders)
+            {
+                //выгрузка папок в лист на окно
+                //listFolders.ItemsSource = folder.Name;
+                nameFolders.Add(folder.Name);
+            }
+            listFolders.ItemsSource = nameFolders;
+        }
+
+        private void MessageOut()
+        {
+            nameMessage.Clear();
+            listMessage.ItemsSource = "";
+            messages = ImapHelper.GetMessagesForFolder(listFolders.SelectedItem.ToString());
+            foreach (var message in messages)
+            {
+                nameMessage.Add(message.From.Address.ToString());
+            }
+            listMessage.ItemsSource = nameMessage;
+        }
+
+        private void writenewmessage_Click(object sender, RoutedEventArgs e)
+        {
+            new WriteMessage().Show();
+        }
+
+        private void listFolders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //MessageCollection messages = ImapHelper.GetMessagesForFolder(folder);
+            //foreach (var message in messages){}
+
+            MessageOut();
+        }
+        private void listMessage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
